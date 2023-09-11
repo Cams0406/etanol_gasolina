7def41f9311f57e6f5d4cd897376f67d2aebb6c5
cams0406 <camily.santos10@etec.sp.gov.br>
2023-09-11 10:13:51 -0300

    arquivo enviado

---
+++ lib/home.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController _controllerEtanol = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+  String _textoResultado="";
+  void _calcular() {
+    double? preocoEtanol = double.tryParse(_controllerEtanol.text);
+    double? preocoGasolina = double.tryParse(_controllerGasolina.text);
+
+    if(preocoEtanol == null || preocoGasolina == null) {
+      //print("Número inválido, digite número maiores que 0 e utilizando (.)");
+   setState(() {
+     _textoResultado = "Número inválido, digite número maiores que 0 e utilizando (.)";
+   });
+   
+    }else{
+      if((preocoEtanol/preocoGasolina)>=0.7){
+        setState(() {
+          _textoResultado = "Melhor abastecer com gasolina";
+          _limpaCampos();
+        });
+      }else{
+        setState(() {
+          _textoResultado = "Melhor abastecer com etanol";
+          _limpaCampos();
+        });
+      }
+    }
+  }
+
+  void _limpaCampos() {
+    _controllerEtanol.clear();
+    _controllerGasolina.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Etanol x Gasolina"),
+        backgroundColor: Colors.red,
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 32),
+                  child: Image(
+                    image: AssetImage("imagens/etagaso.png"),
+                    width: 300,
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(bottom :32),
+                  child: Text(
+                    "Saiba qual a melhor opção para abastecimento do seu carro",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.red,
+                      decoration: TextDecoration.underline),
+                      ),
+                  )),
+                   TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(labelText: "Preço Etanol. Ex 3.50"),
+                    style: TextStyle(fontSize: 25),
+                    controller: _controllerEtanol,
+                   ),
+                   TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(labelText: "Preço Gasolina. Ex 4.50"),
+                    style: TextStyle(fontSize: 25),
+                    controller: _controllerGasolina,
+                   ),
+                   Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(10),
+                        backgroundColor: Colors.blue,
+                        textStyle: TextStyle( 
+                          fontSize: 25
+                        )
+                      ),
+                      onPressed: _calcular,
+                      child: Text(
+                        "Calcular",
+                        style: TextStyle(color: Colors.white),
+                        ),
+                    ),
+                    ),
+                   Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Text(
+                        _textoResultado,
+                        style: TextStyle(fontSize: 25, color: Colors.red),
+                        ),  
+                        ),
+                   )
+              ],
+              ),
+          ),
+        ),
+    );
+  }
+}
